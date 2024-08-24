edbbfa99b8a324c1f7f776a6b9146a446d06343d
Marcelo Rodrigues <marcelorj280@gmail.com>
2024-08-23 21:26:45 -0300

    feat: cria os arquivos responsáveis para rodar os métodos de backtracking e dsatur, medindo o tempo de execução

---
+++ src/dsatur.cpp
@@ -0,0 +1,16 @@
+#include "Graph.h"
+#include <iostream>
+#include <chrono>
+
+using namespace std;
+
+int main (int argc, char* argv[]) {
+  auto start = chrono::high_resolution_clock::now();
+  string inputFile = argv[1];
+  Graph g(inputFile);
+  g.printColors(g.dsaturColoring());
+  auto end = chrono::high_resolution_clock::now();
+  auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
+  cout << "Time: " << duration.count() << " ms" << endl;
+  return 0;
+}
